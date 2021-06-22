6c518ea6a351d6f4547f14f5fd659b53c5da127e
UppathiSuresh <sureshuppathi@gmail.com>
2021-06-22 13:28:50 +0530

    Code clean up

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -47,6 +47,39 @@ namespace BethanysPieShop.Models
                 AllergyInformation = ""
             });
 
+            //    modelBuilder.Entity<Pie>().HasData(new Pie
+            //    {
+            //        PieId = 12,
+            //        Name = "Cake",
+            //        Price = 165M,
+            //        ShortDescription = "Our famous apple pies!",
+            //        LongDescription =
+            //"Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+            //        CategoryId = 1,
+            //        ImageUrl = "https://handletheheat.com/wp-content/uploads/2015/03/Best-Birthday-Cake-with-milk-chocolate-buttercream-SQUARE.jpg",
+            //        InStock = true,
+            //        IsPieOfTheWeek = true,
+            //        ImageThumbnailUrl = "https://handletheheat.com/wp-content/uploads/2015/03/Best-Birthday-Cake-with-milk-chocolate-buttercream-SQUARE.jpg",
+            //        AllergyInformation = ""
+            //    });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 12,
+                Name = "Apple Pie",
+                Price = 12.95M,
+                ShortDescription = "Our famous apple pies!",
+                LongDescription =
+        "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+
             modelBuilder.Entity<Pie>().HasData(new Pie
             {
                 PieId = 2,
